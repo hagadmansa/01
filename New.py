d7278a16803fcbaff6fff2bf53f8a1074c52de31
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-15 02:42:07 +0530

    Update New.py

--- New.py
+++ New.py
@@ -1,202 +1,30 @@
-import random
-from info import PICS
-from pyrogram import filters, Client
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+Choose a specific link
 
-NEW_ABOUT_TEXT = """Hello This command is under testing."""
+<p style="text-align: center;"><button class="button" onClick="window.location.href='https://a.hagadmansa.com/st?api=284ad0efe9a3d5f77d06a23e18bd0c0b157c379b&url=https://download.hagadmansa.com/AgADBg743';">📥 Direct Download</button></p>
 
-NEW_ABOUT_HOME = """Hello This command is under testing."""
+<p style="text-align: center;"><button class="button" onClick="window.location.href='https://a.hagadmansa.com/st?api=284ad0efe9a3d5f77d06a23e18bd0c0b157c379b&url=https://t.me/hagadmansabot?start=ZmlsZV9CUUFEQlFBREJnQUR5RjI1VmdkdGg1TFJILWJPRmdR';">📂 Telegram File</button></p>
 
-RATING_TEXT = """This is rating text."""
+.button{
+	height: 65px;
+  width: 250px;
+	background-color: #61CE70;
+	padding: 25px;
+	border: 1px solid #000;
+        border-radius: 5px;
+	justify-content:center;
+	align-items:center;
+	color: black;
+	font-size: 16px;
+	font-family: Poppins;
+	font-weight: 500;
+	margin: 20px;
+}
 
-SOURCE_TEXT = """This is source text."""
+.button:hover{
+	background-color: #4fb85d;
+	color: black;
+}
 
-DONATE_TEXT = """This is donate text."""
-
-NEW_HELP_TEXT = """<b>🧩 Here is the help of my commands. Send /about to know about me.</b>"""
-
-NEW_HELP_HOME_TEXT = """<b>🧩 Here is the help of my commands. Send /about to know about me.</b>"""
-
-FILE_STREAM_TEXT = """This is file stream text."""
-
-FILE_STORE_TEXT = """This is file store text."""
-
-INSTRUCTONS_TEXT = """This is instructions text."""
-
-TUTORIALS_TEXT = """This is tutorials text."""
-
-WARNING_TEXT = """This is warning text."""
-
-NEW_ABOUT_HOME_BUTTONS = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('⭐️ Rating', callback_data='rating'),
-            InlineKeyboardButton('❤️ Source', callback_data='source'),
-            ],[
-            InlineKeyboardButton('💰 Donate', callback_data='donate')
-            ]]
-       )     
-RATING_BUTTONS = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('🔙 Back', callback_data='new_about_home')
-            ]]
-        )
-SOURCE_BUTTONS = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('🔙 Back', callback_data='new_about_home')
-            ]]
-        )
-DONATE_BUTTONS = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('🔙 Back', callback_data='new_about_home')
-            ]]
-        )
-NEW_HELP_HOME_BUTTONS = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('📥 File Stream', callback_data='file_stream'),
-            InlineKeyboardButton('📦 File Store', callback_data='file_store'),
-            ],[
-            InlineKeyboardButton('⚙️ Instructions', callback_data='instructions'),
-            InlineKeyboardButton('🕹 Tutorials', callback_data='tutorials'),
-            ],[
-            InlineKeyboardButton('⚠️ Warning', callback_data='warning')
-            ]]
-         )
-
-FILE_STREAM_BUTTONS = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('🔙 Back', callback_data='new_help_home')
-            ]]
-        )
-
-FILE_STORE_BUTTONS = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('🔙 Back', callback_data='new_help_home')
-            ]]
-        )
-
-INSTRUCTIONS_BUTTONS = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('🔙 Back', callback_data='new_help_home')
-            ]]
-        )
-
-TUTORIALS_BUTTONS = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('🔙 Back', callback_data='new_help_home')
-            ]]
-        )
-
-WARNING_BUTTONS = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('🔙 Back', callback_data='new_help_home')
-            ]]
-        )
-
-@Client.on_callback_query()
-async def cb_data(bot, update):
-    if update.data == "new_about_home":
-        await update.answer('www.hagadmansa.com')
-        await update.message.edit_text(
-            text=NEW_ABOUT_HOME,
-            disable_web_page_preview=True,
-            reply_markup=NEW_ABOUT_HOME_BUTTONS
-        )
-    elif update.data == "rating":
-        await update.answer('www.hagadmansa.com')
-        await update.message.edit_text(
-            text=RATING_TEXT,
-            disable_web_page_preview=True,
-            reply_markup=RATING_BUTTONS
-        )
-    elif update.data == "source":
-        await update.answer('www.hagadmansa.com')
-        await update.message.edit_text(
-            text=SOURCE_TEXT,
-            disable_web_page_preview=True,
-            reply_markup=SOURCE_BUTTONS
-        )
-    elif update.data == "donate":
-        await update.answer('www.hagadmansa.com')
-        await update.message.edit_text(
-            text=DONATE_TEXT,
-            disable_web_page_preview=True,
-            reply_markup=DONATE_BUTTONS
-        )
-    elif update.data == "new_help_home":
-        await update.answer('www.hagadmansa.com')
-        await update.message.edit_text(
-            text=NEW_HELP_HOME_TEXT,
-            disable_web_page_preview=True,
-            reply_markup=NEW_HELP_HOME_BUTTONS
-        )
-    elif update.data == "file_stream":
-        await update.answer('www.hagadmansa.com')
-        await update.message.edit_text(
-            text=FILE_STREAM_TEXT,
-            disable_web_page_preview=True,
-            reply_markup=FILE_STREAM_BUTTONS
-        )
-    elif update.data == "file_store":
-        await update.answer('www.hagadmansa.com')
-        await update.message.edit_text(
-            text=FILE_STORE_TEXT,
-            disable_web_page_preview=True,
-            reply_markup=FILE_STORE_BUTTONS
-        )
-    elif update.data == "instructions":
-        await update.answer('www.hagadmansa.com')
-        await update.message.edit_text(
-            text=INSTRUCTONS_TEXT,
-            disable_web_page_preview=True,
-            reply_markup=INSTRUCTIONS_BUTTONS
-        )
-    elif update.data == "tutorials":
-        await update.answer('www.hagadmansa.com')
-        await update.message.edit_text(
-            text=TUTORIALS_TEXT,
-            disable_web_page_preview=True,
-            reply_markup=TUTORIALS_BUTTONS
-        )
-    elif update.data == "warning":
-        await update.answer('www.hagadmansa.com')
-        await update.message.edit_text(
-            text=WARNING_TEXT,
-            disable_web_page_preview=True,
-            reply_markup=WARNING_BUTTONS
-        )
-    elif update.data == "close":
-        await update.answer('www.hagadmansa.com')
-        await update.message.delete()
-        try:
-            await update.message.reply_to_message.delete()
-        except:
-            pass
-
-@Client.on_message(filters.command("about"))
-async def about(client, message):
-        await message.reply_photo(
-        photo=random.choice(PICS),
-        caption=(NEW_ABOUT_TEXT),
-        reply_markup=InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('⭐️ Rating', callback_data='rating'),
-            InlineKeyboardButton('❤️ Source', callback_data='source'),
-            ],[
-            InlineKeyboardButton('💰 Donate', callback_data='donate')
-        ]]))
-
-@Client.on_message(filters.command("help"))
-async def start(client, message):
-        await message.reply_photo(
-        photo=random.choice(PICS),
-        caption=(NEW_HELP_TEXT),
-        reply_markup=InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('📥 File Stream', callback_data='file_stream'),
-            InlineKeyboardButton('📦 File Store', callback_data='file_store'),
-            ],[
-            InlineKeyboardButton('⚙️ Instructions', callback_data='instructions'),
-            InlineKeyboardButton('🕹 Tutorials', callback_data='tutorials'),
-            ],[
-            InlineKeyboardButton('⚠️ Warning', callback_data='warning')
-         ]]))
+#new{
+	text-align:center !important;
+}
